83d0fa6f67561ce02f08f842240158df41e1484f
jason-maier-temp <jasonmaier45@gmail.com>
2024-02-27 11:09:19 -0800

    Fetch Exercise Data

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
-import Image from "next/image";
+type Exercise = {
+  id: string;
+  name: string;
+  muscle: string;
+  image: string;
+};
 
-export default function Home() {
+const getExerciseData = async (): Promise<Exercise[]> => {
+  const res = await fetch(
+    "https://storage.googleapis.com/fitbod-web-internal/exercises.json"
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await res.json();
+  return data;
+};
+
+export default async function Home() {
+  const exercises = await getExerciseData();
+
+  console.log(exercises, "<<<< EXERCISES");
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {exercises &&
+        (exercises as Exercise[]).map((exercise) => (
+          <div key={exercise.id}>{exercise.name}</div>
+        ))}
+    </main>
   );
 }
